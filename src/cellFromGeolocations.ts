b627d1dcf99691c82a37be07a118dc6945a7e39b
Markus Tacker <Markus.Tacker@NordicSemi.no>
2022-05-03 16:06:25 +0200

    feat: update dependencies, remove fp-ts and react-leaflet dependencies
    react-leaflet is removed because it has not React 18 release, yet, and it is only used in the demo of this library.
    fp-ts does not provide a real value in this case, and is removed as well.
    
    BREAKING CHANGE: this changes the API.
    
    `cellFromGeolocations()` now returns the cell or `undefined`.
    
    Closes #198
    Closes #197
    Closes #196
    Closes #195
    Closes #194
    Closes #193
    Closes #192
    Closes #191
    Closes #190
    Closes #189

--- src/cellFromGeolocations.ts
+++ src/cellFromGeolocations.ts
@@ -1,4 +1,3 @@
-import { Option, none, some } from 'fp-ts/lib/Option'
 import distance from '@turf/distance'
 
 const calculateDistance = (from: Point, to: Point): number =>
@@ -16,50 +15,52 @@ export type CellGeoLocation = Point & { accuracy: number }
  * - the center is the average of all given locations (within a configurable percentile)
  * - the diameter returned is a circle that includes all given locations (within a configurable percentile), but at least `minCellDiameterInMeters`.
  */
-export const cellFromGeolocations = ({
-	minCellDiameterInMeters,
-	percentile,
-}: {
-	minCellDiameterInMeters: number
-	percentile: number
-}) => (locations: Point[]): Option<CellGeoLocation> => {
-	if (!locations.length) return none
-	if (locations.length === 1)
-		return some({
-			lat: locations[0].lat,
-			lng: locations[0].lng,
-			accuracy: minCellDiameterInMeters,
-		})
-
-	// Location is average of the given percentile of all positions
-	const lats = locations.map(({ lat }) => lat + 90).sort(byNumericValue)
-	const lngs = locations.map(({ lng }) => lng + 180).sort(byNumericValue)
-	const entriesToConsider = Math.round(locations.length * percentile)
-	const start = Math.floor((locations.length - entriesToConsider) / 2)
-	const center = {
-		lat:
-			lats.slice(start, start + entriesToConsider).reduce(sum, 0) /
-				entriesToConsider -
-			90,
-		lng:
-			lngs.slice(start, start + entriesToConsider).reduce(sum, 0) /
-				entriesToConsider -
-			180,
-	}
-
-	// Calculate largest distance, but filter out the given percentile
-	const distances = locations
-		.map((d) => calculateDistance(center, d))
-		.sort(byNumericValue)
-	const significantLargestEntry = Math.floor(locations.length * percentile)
-
-	const accuracy = Math.max(
+export const cellFromGeolocations =
+	({
 		minCellDiameterInMeters,
-		distances[significantLargestEntry],
-	)
+		percentile,
+	}: {
+		minCellDiameterInMeters: number
+		percentile: number
+	}) =>
+	(locations: Point[]): CellGeoLocation | undefined => {
+		if (!locations.length) return undefined
+		if (locations.length === 1)
+			return {
+				lat: locations[0].lat,
+				lng: locations[0].lng,
+				accuracy: minCellDiameterInMeters,
+			}
 
-	return some({
-		...center,
-		accuracy,
-	})
-}
+		// Location is average of the given percentile of all positions
+		const lats = locations.map(({ lat }) => lat + 90).sort(byNumericValue)
+		const lngs = locations.map(({ lng }) => lng + 180).sort(byNumericValue)
+		const entriesToConsider = Math.round(locations.length * percentile)
+		const start = Math.floor((locations.length - entriesToConsider) / 2)
+		const center = {
+			lat:
+				lats.slice(start, start + entriesToConsider).reduce(sum, 0) /
+					entriesToConsider -
+				90,
+			lng:
+				lngs.slice(start, start + entriesToConsider).reduce(sum, 0) /
+					entriesToConsider -
+				180,
+		}
+
+		// Calculate largest distance, but filter out the given percentile
+		const distances = locations
+			.map((d) => calculateDistance(center, d))
+			.sort(byNumericValue)
+		const significantLargestEntry = Math.floor(locations.length * percentile)
+
+		const accuracy = Math.max(
+			minCellDiameterInMeters,
+			distances[significantLargestEntry],
+		)
+
+		return {
+			...center,
+			accuracy,
+		}
+	}
